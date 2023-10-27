26a9d8ffc878a3a0c64c13b869b62ffab4f601e2
singingbush <singingbush@hotmail.com>
2023-10-27 15:48:58 +0100

    improve test code

--- test/source/main.d
+++ test/source/main.d
@@ -22,25 +22,25 @@ import std.string : fromStringz, toStringz;
 version(Windows) {
     string connectionString = "Driver={SQL Server};Server=127.0.0.1;Uid=sa;Pwd=bbk4k77JKH88g54;"; // Windows
 } else {
-    string connectionString = "Driver={ODBC Driver 17 for SQL Server};Server=127.0.0.1,1433;Uid=sa;Pwd=bbk4k77JKH88g54;"; // Unix
+    string connectionString = "Driver={ODBC Driver 17 for SQL Server};Server=127.0.0.1,1433;Uid=sa;Pwd=bbk4k77JKH88g54;TrustServerCertificate=Yes;"; // Unix
     //string connectionString = "Driver=msodbc17;Server=127.0.0.1,1433;Uid=sa;Pwd=bbk4k77JKH88g54;"; // Unix
     //string connectionString = "Driver=FreeTDS;Server=127.0.0.1,1433;Uid=sa;Pwd=bbk4k77JKH88g54;"; // Unix with FreeTDS
 }
 
+SQLHENV env = SQL_NULL_HENV; // environment handle
+SQLHDBC conn = SQL_NULL_HDBC; // connection handle
 
 /*
  * The test code is based on:
  * https://www.easysoft.com/developer/languages/c/odbc_tutorial.html#odbc_api
+ * as well as the Microsoft documentation found here:
+ * https://learn.microsoft.com/en-us/sql/odbc/reference/odbc-programmer-s-reference
+ * https://learn.microsoft.com/en-us/sql/connect/odbc/cpp-code-example-app-connect-access-sql-db
  */
 int main(string[] argv) {
     if(argv.length > 1) {
         connectionString = argv[1];
     }
-
-    SQLHENV env = SQL_NULL_HENV; // environment handle
-    SQLHDBC conn = SQL_NULL_HDBC; // connection handle
-    // SQLHSTMT stmt; // statement handle
-    // SQLHDESC desc; // descriptor handle
 
     char[256] driver;
     char[256] attr;
@@ -52,12 +52,13 @@ int main(string[] argv) {
     SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &env); // Allocate an environment handle
     SQLSetEnvAttr(env, SQL_ATTR_ODBC_VERSION, cast(SQLPOINTER*) SQL_OV_ODBC3, 0); // We want ODBC v3 support
 
+    writeln("Checking installed drivers...");
+
     direction = SQL_FETCH_FIRST;
     while(SQL_SUCCEEDED(ret = SQLDrivers(env, direction, cast(char*) driver, driver.sizeof, &driver_ret, cast(char*) attr, attr.sizeof, &attr_ret))) {
         direction = SQL_FETCH_NEXT;
 
-        //printf("%s - %s\n", cast(char*) driver, cast(char*) attr); // todo: use D style syntax
-        writefln("%s - %s", fromStringz(cast(char*) driver), fromStringz(cast(char*) attr));
+        writefln(" - %s:\t%s", fromStringz(cast(char*) driver), fromStringz(cast(char*) attr));
 
         if (ret == SQL_SUCCESS_WITH_INFO) {
             writeln("\tdata truncation\n");
@@ -73,9 +74,6 @@ int main(string[] argv) {
 
     if(SQL_SUCCEEDED(ret = SQLDriverConnect(conn, null, cast(char*) toStringz(connectionString), SQL_NTS, null, 0, null, SQL_DRIVER_COMPLETE))) {
         SQLCHAR[256] dbms_name, dbms_ver;
-        SQLUINTEGER getdata_support;
-        // SQLUSMALLINT max_concur_act;
-        // SQLSMALLINT string_len;
 
         writeln("Connected");
 
@@ -84,18 +82,22 @@ int main(string[] argv) {
         */
         SQLGetInfo(conn, SQL_DBMS_NAME, cast(SQLPOINTER)dbms_name, dbms_name.sizeof, null);
         SQLGetInfo(conn, SQL_DBMS_VER, cast(SQLPOINTER)dbms_ver, dbms_ver.sizeof, null);
-        // SQLGetInfo(conn, SQL_GETDATA_EXTENSIONS, cast(SQLPOINTER)&getdata_support, 0, 0);
-        // SQLGetInfo(conn, SQL_MAX_CONCURRENT_ACTIVITIES, &max_concur_act, 0, 0);
 
-        writefln("DBMS Name:\t%s", fromStringz(cast(char*) dbms_name));
-        writefln("DBMS Version:\t%s", fromStringz(cast(char*) dbms_ver));
+        writefln(" - DBMS Name:\t%s", fromStringz(cast(char*) dbms_name));
+        writefln(" - DBMS Version:\t%s", fromStringz(cast(char*) dbms_ver));
 
-        // if (max_concur_act == 0) {
-        //     printf("SQL_MAX_CONCURRENT_ACTIVITIES - no limit or undefined\n");
-        // } else {
-        //     printf("SQL_MAX_CONCURRENT_ACTIVITIES = %u\n", max_concur_act);
-        // }
+        SQLUSMALLINT max_concur_act;
+        SQLGetInfo(conn, SQL_MAX_CONCURRENT_ACTIVITIES, cast(SQLPOINTER)max_concur_act, max_concur_act.sizeof, null);
 
+        if (max_concur_act == 0) {
+            writeln("SQLGetData - SQL_MAX_CONCURRENT_ACTIVITIES = no limit or undefined");
+        } else {
+            writefln("SQLGetData - SQL_MAX_CONCURRENT_ACTIVITIES = %u", max_concur_act);
+        }
+
+        SQLUINTEGER getdata_support;
+        SQLGetInfo(conn, SQL_GETDATA_EXTENSIONS, cast(SQLPOINTER)getdata_support, getdata_support.sizeof, null);
+        
         if (getdata_support & SQL_GD_ANY_ORDER) {
             writeln("SQLGetData - columns can be retrieved in any order");
         } else {
@@ -108,14 +110,118 @@ int main(string[] argv) {
             writeln("SQLGetData - columns must be retrieved after last bound one");
         }
 
+
+
+        // Set auto commit on
+        if(SQL_SUCCEEDED(ret = SQLSetConnectAttr(conn, SQL_ATTR_AUTOCOMMIT, cast(SQLPOINTER*) SQL_AUTOCOMMIT_ON, SQL_IS_UINTEGER))) {
+            writeln("SQLSetConnectAttr - AUTOCOMMIT set to ON");
+        } else {
+            stderr.writeln("SQLSetConnectAttr - unable to set AUTOCOMMIT to on");
+        }
+
+
+        execDirect(`SELECT [name] FROM master.dbo.sysdatabases`);
+
+        execDirect("DROP TABLE IF EXISTS [odbc_tst_t1]");
+        execDirect("CREATE TABLE [odbc_tst_t1] ([id] INT NOT NULL IDENTITY(1,1) PRIMARY KEY, [name] VARCHAR(255) NOT NULL, description VARCHAR(max), created DATETIME2 DEFAULT SYSUTCDATETIME() NOT NULL)");
+
+        // INSERT test data (requires IDENTITY_INSERT to be set to ON)
+        execDirect("SET IDENTITY_INSERT [odbc_tst_t1] ON");
+        execDirect("INSERT INTO [odbc_tst_t1] ([id], [name], [description]) VALUES
+                    (1, 'Name 1', 'description 1'),
+                    (2, 'Name 2', 'description 2'),
+                    (3, 'Name 3', 'description 3'),
+                    (4, 'Name 4', 'description 4'),
+                    (5, 'Name 5', 'description 5')");
+
+        execDirect("SET IDENTITY_INSERT [odbc_tst_t1] OFF");
+        execDirect("INSERT INTO [odbc_tst_t1] ([name], [description], [created]) VALUES
+                    ('rand 1', 'Specific Date in 2023', '2023/10/19 15:18:01')");
+
+        // SELECT test data
+        execDirect("SELECT [name] FROM [odbc_tst_t1]");
+
+        // todo: create some select statements for different data types and ensure data can be retrieved + do prepared statements
+
+        // Disconnect from db and free alloacted handles:
         SQLDisconnect(conn);
+        SQLFreeHandle(SQL_HANDLE_DBC, conn);
+        SQLFreeHandle(SQL_HANDLE_ENV, env);
     } else {
-        stderr.writefln("Failed to connect to database. SQL return code: %u", ret);
+        stderr.writefln("Failed to connect to database. SQL return code: %d", ret);
         return 1;
     }
 
-    //SQLRETURN SQLPrepare(SQLHSTMT stmt, SQLCHAR *StatementText, SQLINTEGER TextLength);
-    //SQLRETURN r = SQLAllocHandle(cast(short) SQL_HANDLE_STMT, conn, &stmt);
-
     return 0;
+}
+
+// If a call to SQL reurns -1 (SQL_ERROR) then this function can be called to get the error message
+void writeErrorMessage(SQLHSTMT stmt) {
+    SQLCHAR[6] sqlstate; // A string of 5 characters terminated by a null character. The first 2 characters indicate error class; the next 3 indicate subclass.
+    SQLINTEGER nativeError;
+    SQLCHAR[SQL_MAX_MESSAGE_LENGTH] messageText;
+    SQLSMALLINT bufferLength = messageText.length;
+    SQLSMALLINT textLength;
+
+    SQLRETURN ret = SQLError(
+        env,
+        conn,
+        stmt,
+        &sqlstate[0],
+        &nativeError,
+        &messageText[0],
+        bufferLength,
+        &textLength
+    );
+
+    if(SQL_SUCCEEDED(ret)) {
+        //writefln("SQL State %s, Error %d : %s", fromStringz(sqlstate.ptr), nativeError, fromStringz(messageText.ptr));
+        writefln("SQL State %s, Error %d : %s", fromStringz(cast(char*) sqlstate), nativeError, fromStringz(cast(char*) messageText));
+    }
+}
+
+void execDirect(string sql) {
+    SQLHSTMT stmt;
+    SQLRETURN ret;
+
+    SQLAllocStmt(conn, &stmt);
+
+    if(SQL_SUCCEEDED(ret = SQLExecDirect(stmt, cast(SQLCHAR*) toStringz(sql), SQL_NTS))) {
+        writefln("SQLExecDirect succeeded : %s", sql);
+    } else {
+        stderr.writefln("SQLExecDirect failed. SQL return code: %d", ret);
+        
+        if(ret == SQL_ERROR) {
+            writeErrorMessage(stmt);
+        }
+    }
+
+    SQLINTEGER rowsAffected = 0; // SQLINTEGER is just an alias for int
+    ret = SQLRowCount(stmt, &rowsAffected);
+    if(rowsAffected > 0) {
+        // if a UPDATE, INSERT, or DELETE has been done then we can expect number of rows affected
+        writefln("%d rows affected", rowsAffected);
+    }
+
+    SQLSMALLINT num = 0; // SQLSMALLINT is just an alias for short
+    ret = SQLNumResultCols(stmt, &num); // SQLNumResultCols should be called after statement excuted and if result > 0 there is a result set
+
+    struct Column {
+        SQLINTEGER index;
+        SQLCHAR[32] stringVal;
+    }
+
+    if(num > 0) {
+        writefln("We have a result set with %d columns:", num);
+
+        Column col;
+
+        ret = SQLBindCol(stmt, 1, SQL_C_CHAR, cast(SQLCHAR*) col.stringVal, col.stringVal.sizeof, null);
+
+        while ((ret = SQLFetch(stmt)) == SQL_SUCCESS) {
+            writefln(" - column %d : %s", col.index, fromStringz(cast(char*) col.stringVal));
+        }
+    }
+
+    SQLFreeHandle(SQL_HANDLE_STMT, stmt);
 }
